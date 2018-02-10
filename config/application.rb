45313199dc8ce3b187051bbcb3afe10518165a24
natsuki_shimazu <s2natsuki0701s2@gmail.com>
2018-02-10 17:01:05 +0900

    チャット画面の実装２

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-
-     config.generators do |g|
+    config.generators do |g|
       g.javascripts false
       g.helper false
       g.test_framework false
-
-
     end
   end
 end
-
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   root "messages#index"
-   resources :messages,only: [:index]
+
+  resources :messages, only: [:index]
 
   end
 
