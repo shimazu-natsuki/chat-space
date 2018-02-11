a8549566a124b6e7197f7620a949063864beef36
natsuki_shimazu <s2natsuki0701s2@gmail.com>
2018-02-11 18:26:57 +0900

    group_function routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   root "messages#index"
   resources :messages
   resources :users, only: [:index, :edit, :updat]
+  resources :group, only: [:new, :create, :edit, :updata]do
+    resources :messages, only: [:index, :create]
   end
-
-
+end
 
 
 
